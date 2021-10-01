1928d39593f6714a6cf2b2050bd29cafd0771de2
Angel Legaspi <angel.lugia98@gmail.com>
2021-09-30 21:13:51 -0600

    Implements #11

---
+++ src/main/java/main/Jukebox.java
@@ -0,0 +1,108 @@
+package main;
+
+import com.jagrosh.jdautilities.command.CommandClientBuilder;
+import com.zaxxer.hikari.HikariDataSource;
+import commands.*;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.OnlineStatus;
+import net.dv8tion.jda.api.entities.Activity;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.security.auth.login.LoginException;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class Jukebox
+{
+    //bot setup
+    private static final String prefix = "!";
+    private static final String version = "1.0";
+    private static long uptime;
+
+    //logger
+    private static Logger logger;
+
+    //mysql database (might need it?)
+    private static HikariDataSource ds;
+
+    public static void main(String[] args) throws IOException, IllegalArgumentException
+    {
+        //logger
+        logger = LoggerFactory.getLogger(Jukebox.class);
+
+        //gets tokens and IDs of developers
+        Properties prop = new Properties();
+        FileInputStream propFile = new FileInputStream("config.properties");
+        prop.load(propFile);
+
+        String token = prop.getProperty("bot_token");
+        String ownerId = prop.getProperty("angel_id");
+        String[] coOwnerIds = new String[] {prop.getProperty("andrew_id"), prop.getProperty("daniel_id")};
+
+        //create builder for adding commands and listeners
+        CommandClientBuilder client = new CommandClientBuilder();
+
+        //bot config
+        client.useHelpBuilder(false);
+        client.setPrefix(prefix);
+        client.setOwnerId(ownerId);
+        client.setCoOwnerIds(coOwnerIds[0], coOwnerIds[1]);
+        client.setActivity(Activity.listening("music!"));
+
+        //add commands
+        client.addCommands(
+            new Ping()
+        );
+
+        try
+        {
+            //start tracking uptime
+            uptime = System.currentTimeMillis();
+
+            //start building bot
+            JDABuilder.createDefault(token)
+                .setStatus(OnlineStatus.DO_NOT_DISTURB)
+                .setActivity(Activity.listening("loading!! | !help"))
+                .addEventListeners(client.build())
+                .build();
+        }
+
+        catch(LoginException e)
+        {
+            System.out.println("Unable to login with bot token.");
+            e.printStackTrace();
+        }
+    }
+
+    public static String getVersion()
+    {
+        return version;
+    }
+
+    public static String getPrefix()
+    {
+        return prefix;
+    }
+
+    public static Logger getLogger()
+    {
+        return logger;
+    }
+
+    public static String getUptime()
+    {
+        long ms = System.currentTimeMillis() - uptime;
+        int days, hours, minutes, seconds;
+        String result;
+
+        seconds = (int) ((ms / 1000) % 60);
+        minutes = (int) ((ms / (1000*60)) % 60);
+        hours = (int) ((ms / (1000*60*60)) % 24);
+        days = (int) ((ms / (1000*60*60*24)));
+
+        result = days + " days, " + hours + " hours, " + minutes + " minutes, and " + seconds + " seconds";
+        return result;
+    }
+}
