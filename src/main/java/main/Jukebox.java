9562a4e6cf547f3296e222a164e76b6fd97cee9d
Angel Legaspi <angel.lugia98@gmail.com>
2021-10-14 09:06:15 -0600

    Implements #40

--- src/main/java/main/Jukebox.java
+++ src/main/java/main/Jukebox.java
@@ -6,7 +6,6 @@ import commands.*;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Activity;
-import net.dv8tion.jda.api.requests.GatewayIntent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
