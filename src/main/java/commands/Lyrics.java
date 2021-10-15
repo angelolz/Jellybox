b540215a1032b3143f2be8dcdbb850f47a27774b
shadowbladerx1 <danie4411@gmail.com>
2021-10-15 11:11:07 -0600

    Implemented #25

--- src/main/java/commands/Lyrics.java
+++ src/main/java/commands/Lyrics.java
@@ -2,9 +2,17 @@ package commands;
 
 import com.jagrosh.jdautilities.command.Command;
 import com.jagrosh.jdautilities.command.CommandEvent;
+import core.GLA;
+import net.dv8tion.jda.api.entities.MessageChannel;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 
 public class Lyrics extends Command
 {
+
+    private GLA lyricsGetter;
 
     public Lyrics()
     {
@@ -12,11 +20,44 @@ public class Lyrics extends Command
         this.help = "Returns lyrics for any song";
         this.cooldown = 3;
         this.arguments = "!lyrics <song-name> - Returns <song-name>'s lyrics";
+
+        // Set up for Genius Lyrics API
+        try
+        {
+            // Set up for GLA
+            Properties prop = new Properties();
+            FileInputStream propFile = new FileInputStream("config.properties");
+            prop.load(propFile);
+            lyricsGetter = new GLA(prop.getProperty("gla_id"), prop.getProperty("gla_access_token"));
+        }
+        catch (IOException e)
+        {
+            System.out.println("Config file error!");
+        }
     }
     @Override
     protected void execute(CommandEvent event)
     {
+        MessageChannel channel = event.getChannel();
+        String[] lyrics = lyricsGetter.search("Lose Yourself").get(0).getText().split(System.lineSeparator());
+        StringBuilder sb = new StringBuilder();
+        System.out.println("Start");
+        for(String line: lyrics)
+        {
+            if((sb.length() + line.length()) > 1900)
+            {
+                channel.sendMessage(sb.toString()).queue();
+                sb = new StringBuilder();
+            }
+            else
+            {
+                sb.append(line).append("\n");
+            }
+        }
 
+        if(!sb.toString().equals("")){
+            channel.sendMessage(sb).queue();
+        }
     }
 
 
