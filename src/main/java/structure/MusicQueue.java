f3720c5416995fdb854a51f5eeeb70471bdcc9c7
AndrewCarlson1996 <70498953+AndrewCarlson1996@users.noreply.github.com>
2021-10-17 18:14:51 -0600

    Implemented requested changes

--- src/main/java/structure/MusicQueue.java
+++ src/main/java/structure/MusicQueue.java
@@ -1,19 +1,17 @@
 package structure;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.Queue;
-import java.net.URL;
+
+import java.util.LinkedList;
 
 /**
  * 
  * @author Andrew Carlson
  */
-public class MusicQueue implements Queue<Song>{
+public class MusicQueue extends LinkedList<Song>{
 
     
     private int size;
     private boolean isEmpty;
-    private Queue<Song> queue;
+    private LinkedList<Song> queue;
 
 
     public MusicQueue(){
@@ -58,12 +56,6 @@ public class MusicQueue implements Queue<Song>{
     }
 
     @Override
-    public Iterator<Song> iterator() {
-        return queue.iterator();
-    }
-
-    
-    @Override
     public Object[] toArray() {
         return queue.toArray();
     }
@@ -79,77 +71,35 @@ public class MusicQueue implements Queue<Song>{
     }
 
     @Override
-    public boolean containsAll(Collection c) {
-        return queue.contains(c);
-    }
-
-    @Override
-    public boolean addAll(Collection c) {
-        return queue.addAll(c);
-    }
-
-    @Override
-    public boolean removeAll(Collection E) {
-        return queue.removeAll(E);
-    }
-
-    @Override
-    public boolean retainAll(Collection c) {
-        return queue.retainAll(c);
-    }
-
-    @Override
     public void clear() {
         queue.clear();
     }
 
     @Override
     public boolean add(Song e) {
-        String url = e.GetUrl();
-        if(isValid(url)){
-            queue.add(e);
+        if(queue.add(e)){
             return true;
         }
-        return false;
-    }
-
-    @Override
-    public boolean offer(Song e) {
-        // TODO Auto-generated method stub
         return false;
     }
 
     @Override
     public Song remove() {
-        // TODO Auto-generated method stub
         return null;
     }
 
     @Override
     public Song poll() {
-        // TODO Auto-generated method stub
         return null;
     }
 
     @Override
     public Song element() {
-        // TODO Auto-generated method stub
         return null;
     }
 
     @Override
     public Song peek() {
-        // TODO Auto-generated method stub
         return null;
-    }
-    
-    public boolean isValid(String url){
-        try{
-            new URL(url).toURI();
-            return true;
-        }
-        catch(Exception e){
-            return false;
-        }
     }
 }
