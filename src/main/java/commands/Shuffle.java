69ed891d3e2259e2bd481334c9381a203194b5c9
AndrewCarlson1996 <70498953+AndrewCarlson1996@users.noreply.github.com>
2021-11-14 13:20:29 -0700

    Closes #154 (#158)

---
+++ src/main/java/commands/Shuffle.java
@@ -0,0 +1,45 @@
+package commands;
+import java.util.Collections;
+import com.jagrosh.jdautilities.command.Command;
+import com.jagrosh.jdautilities.command.CommandEvent;
+import music.PlayerManager;
+import music.TrackScheduler;
+import net.dv8tion.jda.api.entities.GuildVoiceState;
+
+public class Shuffle extends Command
+{
+    public Shuffle(){
+        this.name = "Shuffle";
+        this.help = "Shuffles the songs that are in the queue (if any).";
+        this.cooldown = 3;
+    }
+
+    @Override
+    protected void execute(CommandEvent commandEvent){
+        GuildVoiceState userVoiceState = commandEvent.getMember().getVoiceState();
+        GuildVoiceState selfVoiceState = commandEvent.getSelfMember().getVoiceState();
+
+        if(!selfVoiceState.inVoiceChannel())
+            commandEvent.reply(":x: | I'm not even in a voice channel!");
+
+        else if(!userVoiceState.inVoiceChannel())
+            commandEvent.reply(":x: | You need to be in a voice channel to use this command!");
+
+        else if(selfVoiceState.inVoiceChannel() && !userVoiceState.getChannel().equals(selfVoiceState.getChannel()))
+            commandEvent.reply(":x: | You need to be in the same voice channel as me for this command to work!");
+
+        else
+        {
+            TrackScheduler scheduler  = PlayerManager.getInstance().getMusicManager(commandEvent.getGuild()).getScheduler();
+
+            if(scheduler.getQueue().size() > 1)
+            {
+                Collections.shuffle(scheduler.getQueue());
+            }
+            else
+            {
+                commandEvent.reply("error");
+            }
+        }
+    }
+}
